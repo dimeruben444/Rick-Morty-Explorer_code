379c6f650e74962a76e90498df0471dfacc1ca52
Ruben <dimeruben444@gmail.com>
2026-03-01 13:25:10 +0100

    feat(pagination)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
+import "./sass/style.scss";
 
-import './sass/style.scss'
+let urlApi = "https://rickandmortyapi.com/api/character";
 
-let urlApi = 'https://rickandmortyapi.com/api/character'
-
-let arrPersonajes = []
-
+let arrPersonajes = [];
+/* 
 fetch(urlApi)
     .then((response)=> response.json())
     .then(data => {
@@ -12,29 +11,85 @@ fetch(urlApi)
         mostrarpersonajes(arrPersonajes)
 })
  
+ */
+let app = document.querySelector("#app");
 
-let app = document.querySelector('#app')
-
-
-function mostrarpersonajes(arrPersonajes){
-    arrPersonajes.forEach( personaje => {
-        let newCard = document.createElement('div')
-        newCard.innerHTML =`
-            <div class="character-card">
-      
+function mostrarpersonajes(arrPersonajes) {
+  arrPersonajes.forEach((personaje) => {
+    let newCard = document.createElement("div");
+    newCard.innerHTML = `
                 <div class="img-container">
                     <img src= "${personaje.image}" alt="">
-                    <button class="status">${personaje.status}</button>
+                    <button class="status ${personaje.status} ">${personaje.status}</button>
                 </div>
                 <div class="info-container">
                     <h3 class="character-name">${personaje.name}</h3>
                     <p class="character-specie">${personaje.gender} - ${personaje.species}</p>
-                    <p class="character-location">${personaje.location.name} </p>
+                    <p class="character-location"><span>📍</span> ${personaje.location.name} </p>
                 </div>
-            </div>
-        
-        `
-        
-        app.appendChild(newCard)
-    });
+        `;
+    newCard.classList.add("card");
+    app.appendChild(newCard);
+  });
 }
+
+let pagination = document.querySelector("#pagination");
+
+let paginaActual = 1;
+let totalPaginas = 1;
+
+
+
+const cargarPersonajes = async (numeroPagina) => {
+  document.getElementById("app").innerHTML = "<p>Cargando...</p>";
+
+  let urlApi = `https://rickandmortyapi.com/api/character/?page=${numeroPagina}`;
+
+  await fetch(urlApi)
+    .then((response) => response.json())
+    .then((data) => {
+
+      arrPersonajes = data.results;
+      paginaActual = numeroPagina;
+      totalPaginas = data.info.pages;
+      
+
+      document.getElementById("app").innerHTML = "";
+      
+      mostrarpersonajes(arrPersonajes);
+    });
+};
+
+cargarPersonajes();
+
+console.log(totalPaginas)
+function paginaSiguiente(){
+    if(paginaActual < totalPaginas){
+        cargarPersonajes(paginaActual + 1)
+        console.log('sigiente')
+    }
+
+}
+
+
+function paginaAnterior(){
+    if(paginaActual > 1 ){
+        cargarPersonajes(paginaActual - 1)
+    }
+    
+}
+
+const buttonSiguiente= document.getElementById('siguiente')
+const buttonAnterior= document.getElementById('anterior')
+
+buttonSiguiente.addEventListener('click',(e)=>{
+    e.preventDefault()
+    
+    paginaSiguiente()
+   
+})
+
+buttonAnterior.addEventListener('click',(e)=>{
+    e.preventDefault()
+    paginaAnterior()
+})
